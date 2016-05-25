7addd6e0ff72a71fd23550778ac3910e002b19bc
Coco <zhuxiaodi0331@gmail.com>
2016-05-24 21:11:53 -0400

    Update sql_conn.java

--- src/main/sql_conn.java
+++ src/main/sql_conn.java
@@ -5,11 +5,11 @@ import java.sql.DriverManager;
 
 public class sql_conn {
 	
-	private static String db_name = "xbrl";
-	private static String db_user = "xbrl";
-	private static String db_pwd = "xbrl1234";
-	private static String db_host = "155.246.104.69";
-	private static String db_port = "5432";
+	private static String db_name = "";
+	private static String db_user = "";
+	private static String db_pwd = "";
+	private static String db_host = "";
+	private static String db_port = "";
 	
 	public static Connection sqlconn(){
 		Connection c = null;
